e5f8b64291029cf60288b69e18a5748ddc1efa12
simon44LH <103035547+simon44LH@users.noreply.github.com>
2022-11-18 09:44:03 +0100

    added the radio buttons

--- src/Form.jsx
+++ src/Form.jsx
@@ -8,6 +8,7 @@ export default function Form() {
     email: "",
     comments: "",
     isFriendly: true,
+    employment: "",
   });
 
   function handleChange(event) {
@@ -60,6 +61,43 @@ export default function Form() {
         name="isFriendly"
       />
       <label htmlFor="isFriendly">Are you friendly?</label>
+
+      <fieldset>
+        <legend>Current employment status</legend>
+
+        <input
+          type="radio"
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          checked={formData.employment === "unemployed"}
+          onChange={handleChange}
+        />
+        <label htmlFor="unemployed">Unemployed</label>
+        <br />
+
+        <input
+          type="radio"
+          id="part-time"
+          name="employment"
+          value="part-time"
+          checked={formData.employment === "part-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="part-time">Part-time</label>
+        <br />
+
+        <input
+          type="radio"
+          id="full-time"
+          name="employment"
+          value="full-time"
+          checked={formData.employment === "full-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="full-time">Full-time</label>
+        <br />
+      </fieldset>
     </form>
   );
 }
